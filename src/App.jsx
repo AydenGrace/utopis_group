7130c08e0d5ffe15e2aa8a05d091a157a0087e37
maximilien <ro.maximilien@gmail.com>
2025-04-11 15:10:05 +0200

    update footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Landing from "./components/landing/Landing";
 import Header from "./components/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
     <>
       <Header />
       <Landing></Landing>
+      <Footer/>
     </>
   );
 }
