b5bf0617ea98acccbf8c9287ad3a62fccafb71f4
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-04-11 14:57:37 +0200

    landing correction

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -3,7 +3,7 @@ export default function Landing() {
   return (
     <div
       style={{
-        width: "100vw",
+        width: "100%",
         height: "100vh",
         backgroundImage:
           "url('https://images.pexels.com/photos/705075/pexels-photo-705075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
