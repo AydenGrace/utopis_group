397294130492e5aaa8770c522505b5ff5d8e1743
maximilien <ro.maximilien@gmail.com>
2025-04-11 15:37:37 +0200

    je cree un conflit

--- src/components/Apropos.jsx
+++ src/components/Apropos.jsx
@@ -4,7 +4,7 @@ export default function Apropos() {
   return (
     <>
       <div>Section a propos</div>
-      <div>Section a propos</div>
-    </>
+    <span>Ceci est la section a propos</span>
+      </>
   );
 }
