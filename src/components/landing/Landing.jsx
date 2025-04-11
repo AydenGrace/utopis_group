d32c7818313b04f6330f225183fe163078d75e89
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-04-11 14:31:49 +0200

    landing creation

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+export default function Landing() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundImage:
+          "url('https://images.pexels.com/photos/705075/pexels-photo-705075.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    ></div>
+  );
+}
