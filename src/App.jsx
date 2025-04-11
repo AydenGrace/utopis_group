a6552ad706c213a4ddd34c9e4bdd060f81e0bbcc
Azarges <azarges@hotmail.fr>
2025-04-11 14:52:46 +0200

    Ajout section apropos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Landing from "./components/landing/Landing";
 import Header from "./components/Header";
+import Apropos from "./components/Apropos";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
       <Landing></Landing>
+      <Apropos />
     </>
   );
 }
