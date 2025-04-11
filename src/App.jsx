7508fe2ff36e2c1acb6103c83cbc7dd7f9b92bb4
DBrandon2 <137073865+DBrandon2@users.noreply.github.com>
2025-04-11 14:40:46 +0200

    Header in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Landing from "./components/landing/Landing";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <Landing></Landing>
     </>
   );
